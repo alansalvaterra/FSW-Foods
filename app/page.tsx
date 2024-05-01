7d859f94b933f664c702c37b43d36c6ddbbf65c8
alansalvaterra <alan.salvaterra@gmail.com>
2024-04-30 23:26:54 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <>
       <Header />
+
       <div className="px-5 pt-6">
         <Search />
       </div>
@@ -13,6 +15,16 @@ const Home = () => {
       <div className="px-5 pt-6">
         <CategoryList />
       </div>
+
+      <Image
+        src="/promo-banner01.png"
+        alt="Banner com oferta promocional de até 30% de desconto em pizzas"
+        width={0}
+        height={0}
+        className="h-auto w-full object-contain px-5 pt-6"
+        sizes="100vw"
+        quality={100}
+      />
     </>
   );
 };
