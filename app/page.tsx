a45e69a9110bb4cec0f2b5202c064a2ddcb37e04
alansalvaterra <alan.salvaterra@gmail.com>
2024-04-30 21:28:24 -0300

    feat: add initial header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "./_components/header";
+
 const Home = () => {
   return (
-    <div className="p-4 bg-red-500">
-      <h1>Hello Tailwind</h1>
-    </div>
+    <>
+      <Header></Header>
+    </>
   );
 };
 
