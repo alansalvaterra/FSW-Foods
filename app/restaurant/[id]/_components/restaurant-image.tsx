240cd0cac5bd2a957c948b3dabf224ac6825b3e8
alansalvaterra <alan.salvaterra@gmail.com>
2024-05-01 22:37:10 -0300

    feat: add restaurant page

---
+++ app/restaurant/[id]/_components/restaurant-image.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Button } from "@/app/_components/ui/button";
+import type { Restaurant } from "@prisma/client";
+import { ChevronLeftIcon, HeartIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+interface RestaurantImageProps {
+  restaurant: Pick<Restaurant, "imageUrl" | "name">;
+}
+
+const RestaurantImage = ({ restaurant }: RestaurantImageProps) => {
+  const router = useRouter();
+
+  const handleBackClick = () => router.back();
+
+  return (
+    <div className="relative h-[215px] w-full">
+      {/* IMAGEM RESTAURANTE */}
+      <Image
+        className="object-cover"
+        src={restaurant?.imageUrl}
+        alt={restaurant?.name}
+        layout="fill"
+      />
+
+      {/* BOTAO VOLTAR */}
+      <Button
+        className="absolute left-4 top-4 rounded-full border-2 bg-white text-foreground hover:text-white"
+        size="icon"
+        onClick={handleBackClick}
+      >
+        <ChevronLeftIcon />
+      </Button>
+
+      {/* BOTAO FAVORITAR */}
+      <Button
+        size="icon"
+        className="absolute right-4 top-4 rounded-full bg-gray-700"
+      >
+        <HeartIcon size={20} className="fill-white" />
+      </Button>
+    </div>
+  );
+};
+
+export default RestaurantImage;
